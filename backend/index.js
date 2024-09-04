d004babcacfb0496957a9e83c45bc5cb29e1867d
cavalcanteprofissional <cavalcanteprofissional@outlook.com>
2024-09-04 09:38:09 -0300

    correção index

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors =require('cors')
+
+const app = express()
+
+
+
+ //config JSON  response
+ app.use(express.json())
+
+ //solve CORS
+ app.use(cors({ credentials: true, origin: 'http://localhost:3000'}))
+
+ //Public folder for Images
+ app.use(express.static('public'))
+
+//Routes
+const UserRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
+
+app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
+
+
+app.listen(5000)
