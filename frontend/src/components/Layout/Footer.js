8b4316964964c580438ba9617af8cc22f6f308cd
Raimundo Bruno <brunojust55@gmail.com>
2024-09-15 09:30:57 -0300

    Layout

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return  (
+
+        <footer className={styles.footer}>
+            <p>
+                <span className="bold">Get A Pet</span> &copy; 2024
+            </p>
+
+        </footer>
+    )
+}
+
+export default Footer
